592b102f9cd284f4a921d5be2b0e60c9dcb43f95
Logan <lruzzier20@stuy.edu>
2021-01-05 18:58:57 -0500

    added weirder() helper function and test cases

--- PigLatin.java
+++ PigLatin.java
@@ -2,6 +2,16 @@ import java.util.*;
 public class PigLatin{
   public static void main(String[] args){
     System.out.println(pigLatinSimple("david"));
+    System.out.println(pigLatinSimple("avid"));
+    System.out.println(pigLatinSimple("eavid"));
+    System.out.println(pigLatinSimple("iavid"));
+    System.out.println(pigLatinSimple("oavid"));
+    System.out.println(pigLatinSimple("uavid"));
+    System.out.println("-----------------------");
+    System.out.println(pigLatin("black"));
+    System.out.println(pigLatin("alack"));
+    System.out.println(pigLatin("slim"));
+    System.out.println(pigLatin("lol"));
   }
 
   public static boolean weird(char[] sample, String term){
@@ -11,6 +21,15 @@ public class PigLatin{
       if(lookfor==sample[i]){answer=true;}
     }
     return answer;
+  }
+
+  public static boolean weirder(String[] digraphs, String check){
+    String lookerfor = check.substring(0,2);
+    boolean statement = false;
+    for(int i=0;i<digraphs.length;i++){
+      if(lookerfor.equals(digraphs[i])){statement=true;}
+    }
+    return statement;
   }
 
   public static String pigLatinSimple(String s){
@@ -25,7 +44,7 @@ public class PigLatin{
                               "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr",
                               "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp",
                               "st", "sw", "th", "tr", "tw", "wh", "wr"};
-    if(weirder(c, st)){return st.subString(3) + st.subString(0,3) + "ay";}
+    if(weirder(c, st)){return st.substring(2) + st.substring(0,2) + "ay";}
     if(weird(b, st)){return st + "hay";}
     return st.substring(1) + st.charAt(0) + "ay";
   }
