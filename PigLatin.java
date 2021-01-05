a464b70acd8d2b5b271a9347a4fc93c006c3ae8a
Logan <lruzzier20@stuy.edu>
2021-01-05 18:49:07 -0500

    finished pigLatinII, minus weirder() helper function

--- PigLatin.java
+++ PigLatin.java
@@ -19,5 +19,14 @@ public class PigLatin{
     return s.substring(1) + s.charAt(0) + "ay";
   }
 
-
+  public static String pigLatin(String st){
+    char[] b = new char[]{'a','e','i','o','u'};
+    String[] c = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl",
+                              "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr",
+                              "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp",
+                              "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    if(weirder(c, st)){return st.subString(3) + st.subString(0,3) + "ay";}
+    if(weird(b, st)){return st + "hay";}
+    return st.substring(1) + st.charAt(0) + "ay";
+  }
 }
