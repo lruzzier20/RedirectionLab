204d29475b8f5c7ba51f7c2237011f213190a848
Logan <lruzzier20@stuy.edu>
2021-01-05 19:10:45 -0500

    fixed issues of small input and capitalization

--- PigLatin.java
+++ PigLatin.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class PigLatin{
   public static void main(String[] args){
-    System.out.println(pigLatinSimple("david"));
+    System.out.println(pigLatinSimple("David"));
     System.out.println(pigLatinSimple("avid"));
     System.out.println(pigLatinSimple("eavid"));
     System.out.println(pigLatinSimple("iavid"));
@@ -12,6 +12,8 @@ public class PigLatin{
     System.out.println(pigLatin("alack"));
     System.out.println(pigLatin("slim"));
     System.out.println(pigLatin("lol"));
+    System.out.println(pigLatin("sl"));
+    System.out.println(pigLatin("no"));
   }
 
   public static boolean weird(char[] sample, String term){
@@ -33,19 +35,26 @@ public class PigLatin{
   }
 
   public static String pigLatinSimple(String s){
+    String fix = s.toLowerCase();
     char[] a = new char[]{'a','e','i','o','u'};
-    if(weird(a, s)){return s + "hay";}
-    return s.substring(1) + s.charAt(0) + "ay";
+    if(weird(a, fix)){return fix + "hay";}
+    return fix.substring(1) + fix.charAt(0) + "ay";
   }
 
   public static String pigLatin(String st){
+    st = st.toLowerCase();
     char[] b = new char[]{'a','e','i','o','u'};
     String[] c = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl",
                               "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr",
                               "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp",
                               "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    if(st.length()<=1){return st + "ay";}
     if(weirder(c, st)){return st.substring(2) + st.substring(0,2) + "ay";}
     if(weird(b, st)){return st + "hay";}
     return st.substring(1) + st.charAt(0) + "ay";
+  }
+
+  public static String pigLatinBest(String s){
+    return "uh";
   }
 }
